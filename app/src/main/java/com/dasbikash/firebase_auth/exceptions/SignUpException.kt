d1ae3407ceafb3bd0f08e0135eb66f0cbc3be49b
Bikash Das <bikashdaseee@gmail.com>
2020-04-16 06:32:40 +0600

    Init commit.

---
+++ app/src/main/java/com/dasbikash/firebase_auth/exceptions/SignUpException.kt
@@ -0,0 +1,8 @@
+package com.dasbikash.firebase_auth.exceptions
+
+class SignUpException:RuntimeException {
+    constructor() : super()
+    constructor(message: String?) : super(message)
+    constructor(message: String?, cause: Throwable?) : super(message, cause)
+    constructor(cause: Throwable?) : super(cause)
+}
